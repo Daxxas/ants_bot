6f04910430f535f90dfbede87510f4a376101f5c
Bigaston <bonsoir@bigaston.me>
2023-04-13 11:28:48 +0200

    Add all the basic nodes

---
+++ bt/CompositeNode.h
@@ -0,0 +1,19 @@
+#include <list>
+#include "Node.h"
+
+class CompositeNode : public Node
+{ //  This type of Node follows the Composite Pattern, containing a list of other Nodes.
+private:
+  std::list<Node *> children;
+
+public:
+  const std::list<Node *> &getChildren() const
+  {
+    return children;
+  }
+
+  void addChild(Node *child)
+  {
+    children.emplace_back(child);
+  }
+};
---
+++ bt/Node.h
@@ -0,0 +1,5 @@
+class Node
+{ // This class represents each node in the behaviour tree.
+public:
+	virtual bool run() = 0;
+};
