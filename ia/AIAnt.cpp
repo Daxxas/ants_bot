e0bf1eca58a42a714a3534705bc751214dca7a3c
Bigaston <bonsoir@bigaston.me>
2023-04-13 15:55:37 +0200

    End Soldier Setup

--- ia/AIAnt.cpp
+++ ia/AIAnt.cpp
@@ -7,7 +7,12 @@
 // Include all leef
 #include "I_HasMeetingPoint.h"
 #include "I_IsOnMeetingPoint.h"
+#include "I_HasEnnemyNearby.h"
+#include "I_IsMoreThanEnnemy.h"
+
 #include "A_WalkToMeetingPoint.h"
+#include "A_DeleteMeetingPoint.h"
+#include "A_WalkToEnnemy.h"
 
 AIAnt::AIAnt(Ant &_ant) : ant(_ant)
 {
@@ -18,7 +23,7 @@ AIAnt::AIAnt(Ant &_ant) : ant(_ant)
   I_HasMeetingPoint hasMeetingPoint = I_HasMeetingPoint();
   soldier.addChild(hasMeetingPoint);
 
-  // Soldier -> MeetingPoint
+  // Soldier/MeetingPoint
   Selector meetingPoint = Selector();
   soldier.addChild(meetingPoint);
 
@@ -27,6 +32,27 @@ AIAnt::AIAnt(Ant &_ant) : ant(_ant)
 
   A_WalkToMeetingPoint walkToMeetingPoint = A_WalkToMeetingPoint();
   meetingPoint.addChild(walkToMeetingPoint);
+
+  // Soldier/Attack
+  Sequence attack = Sequence();
+  soldier.addChild(attack);
+
+  // Soldier/Attack/Keep Status
+  Selector keepStatus = Selector();
+  attack.addChild(keepStatus);
+
+  I_HasEnnemyNearby hasEnnemyNearby = I_HasEnnemyNearby();
+  keepStatus.addChild(hasEnnemyNearby);
+
+  A_DeleteMeetingPoint deleteMeetingPoint = A_DeleteMeetingPoint();
+  keepStatus.addChild(deleteMeetingPoint);
+
+  // Soldier/Attack
+  I_IsMoreThanEnnemy isMoreThanEnnemy = I_IsMoreThanEnnemy();
+  attack.addChild(isMoreThanEnnemy);
+
+  A_WalkToEnnemy walkToEnnemy = A_WalkToEnnemy();
+  attack.addChild(walkToEnnemy);
 
   // End and run the Behaviour Tree
   NodeStatus status = root.run(ant);
