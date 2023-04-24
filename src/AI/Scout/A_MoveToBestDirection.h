0a275c96b6e16c81106f801e5828ac87485597a8
CleryJa <daxxas158@gmail.com>
2023-04-24 12:44:04 +0200

    call astar in ant move to best direction

--- src/AI/Scout/A_MoveToBestDirection.h
+++ src/AI/Scout/A_MoveToBestDirection.h
@@ -1,12 +1,14 @@
 #include "../../bt/BT_Node.h"
+#include "../AStar.h"
 
 class A_MoveToBestDirection : public BT_Node
 {
 public:
-  virtual NodeStatus run(Ant &ant, State &state) override
-  {
-    // TODO: Implement this method.
+    virtual NodeStatus run(Ant &ant, State &state) override
+    {
+        Location target = Location(0,0);
+        std::vector<Location> path = AStar::FindPath(state, ant.location, target);
 
-    return NodeStatus::RUNNING;
-  }
+        return NodeStatus::RUNNING;
+    }
 };
